f12366490b74848aa023023ef644e50db6b7783d
Kat Young <kathryn.ae.young@gmail.com>
2022-01-30 16:42:08 +0000

    Moveable marker
    index.html - Added title and instruction text.
    style.css - Added font.
    sketch.js - Basic setup - marker which can be moved by arrows and mouse, limited to within canvas.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,72 @@
+//size variables - using 1px = 1cm
+// for reference, gen6 teaching room is 7m x 5.6m x 2.6m
+let canvasSizeX = 700; // 7m wide
+let canvasSizeY = 460; //4.6m deep
+let markerSize = 15; //person head ~15cm wide?
+
+// movement variables
+let markerMovementInc = 10; //10cm increments
+let markerPermitMovement = [1, 1, 1, 1]; //Left right up down
+
+// starting position variables - bottom right
+let markerPosX = canvasSizeX - 50;
+let markerPosY = canvasSizeY - 10;
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(canvasSizeX, canvasSizeY);
 }
 
 function draw() {
-    background(220);
+    background(255);
+    noFill();
+    stroke(0);
+    rect(0, 0, canvasSizeX, canvasSizeY);
+
+    drawDoor();
+    drawMarker(markerPosX, markerPosY, markerSize);
+}
+
+//Draw marker at given position and size
+function drawMarker(xPos, yPos, size) {
+    fill(150);
+    noStroke();
+    ellipse(xPos, yPos, size, size);
+}
+
+//Draw door in bottom right corner
+function drawDoor() {
+    //standard door width ~ 75cm - therefore needs diameter of 150
+    arc(canvasSizeX - 5, canvasSizeY, 150, 150, PI, PI * (2.5 / 2), PIE);
+}
+
+//Use arrow keys to move marker around in increments
+function keyPressed() { //only when pressed and released rather than pressed and held
+
+    // determine where marker can go based on current position - is the next increment going to take outside of walls?
+    if (markerPosX - markerMovementInc <= 0) { markerPermitMovement[0] = 0; } //can't go left
+    if (markerPosX + markerMovementInc >= canvasSizeX) { markerPermitMovement[1] = 0; } //can't go right
+    if (markerPosY - markerMovementInc <= 0) { markerPermitMovement[2] = 0; } //can't go up
+    if (markerPosY + markerMovementInc >= canvasSizeY) { markerPermitMovement[3] = 0; } //can't go down
+
+    //change marker position based on permitted movements
+    if (keyCode === RIGHT_ARROW && markerPermitMovement[1]) {
+        markerPosX = markerPosX + markerMovementInc;
+    } else if (keyCode === LEFT_ARROW && markerPermitMovement[0]) {
+        markerPosX = markerPosX - markerMovementInc;
+    } else if (keyCode === UP_ARROW && markerPermitMovement[2]) {
+        markerPosY = markerPosY - markerMovementInc;
+    } else if (keyCode === DOWN_ARROW && markerPermitMovement[3]) {
+        markerPosY = markerPosY + markerMovementInc;
+    }
+
+    // reset permitted movements for next key press
+    markerPermitMovement = [1, 1, 1, 1];
+}
+
+//Click mouse to move to new position
+function mousePressed() {
+    if (mouseX <= canvasSizeX && mouseX >= 0 && mouseY >= 0 && mouseY <= canvasSizeY) {
+        markerPosX = mouseX;
+        markerPosY = mouseY;
+    }
 }
