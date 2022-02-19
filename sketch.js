841567d8b71e0260a0423c72b307ac22849f15ad
Kat Young <kathryn.ae.young@gmail.com>
2022-02-19 17:21:40 +0000

    WASD, roomSize
    Switched arrow keys for WASD to avoid autofocus on radio buttons. Refactored into roomSize and canvasSize

--- sketch.js
+++ sketch.js
@@ -8,17 +8,20 @@ https://github.com/tibbakoi
 
 //size variables - using 1px = 1cm
 // for reference, gen6 teaching room is 7m x 5.6m x 2.6m
-let canvasSizeX = 500; // 7m wide
-let canvasSizeY = 460; //4.6m deep
+let roomSizeX = 700; // 7m wide
+let roomSizeY = 460; //4.6m deep
 let markerSize = 15; //person head ~15cm wide?
+
+let canvasSizeX = roomSizeX;
+let canvasSizeY = roomSizeY + 75; //extra room at the bottom for buttons etc
 
 // movement variables
 let markerMovementInc = 5; //5cm increments
 let markerPermitMovement = [1, 1, 1, 1]; //Left right up down
 
 // starting position variables - bottom right
-let markerPosX = canvasSizeX - 50;
-let markerPosY = canvasSizeY - 10;
+let markerPosX = roomSizeX - 50;
+let markerPosY = roomSizeY - 10;
 
 // audio variables
 let oscX = new p5.Oscillator('sine');
@@ -37,10 +40,11 @@ function setup() {
     oscX.amp(0);
     oscX.freq(lowestFreq * currentHarmonic);
 
-    playButton = createButton('toggle play').position(0, 0, "relative");
+    playButton = createButton('toggle play').position(10, roomSizeY + 125);
     playButton.mousePressed(playPauseAudio);
+    playButton.parent('demo');
 
-    harmonicSelectRadio = createRadio().position(0, 0, "relative");
+    harmonicSelectRadio = createRadio().position(10, roomSizeY + 150);
     harmonicSelectRadio.option('1');
     harmonicSelectRadio.option('2');
     harmonicSelectRadio.option('3');
@@ -54,28 +58,29 @@ function draw() {
     noFill();
     stroke(0);
     rect(0, 0, canvasSizeX, canvasSizeY);
+    rect(0, 0, roomSizeX, roomSizeY);
     drawDoor();
 
     //changing elements
 
     //changing marker location on keyPressed status rather than keyPressed function allows for press and hold
     if (keyIsPressed) {
-        //Use arrow keys to move marker around in increments
-        if (keyCode === LEFT_ARROW || keyCode === RIGHT_ARROW || keyCode === UP_ARROW || keyCode === DOWN_ARROW) {
+        //Use WASD keys to move marker around in increments
+        if (keyCode === 65 || keyCode === 68 || keyCode === 87 || keyCode === 83) {
             // determine where marker can go based on current position - is the next increment going to take outside of walls?
             if (markerPosX - markerMovementInc <= 0) { markerPermitMovement[0] = 0; } //can't go left
-            if (markerPosX + markerMovementInc >= canvasSizeX) { markerPermitMovement[1] = 0; } //can't go right
+            if (markerPosX + markerMovementInc >= roomSizeX) { markerPermitMovement[1] = 0; } //can't go right
             if (markerPosY - markerMovementInc <= 0) { markerPermitMovement[2] = 0; } //can't go up
-            if (markerPosY + markerMovementInc >= canvasSizeY) { markerPermitMovement[3] = 0; } //can't go down
+            if (markerPosY + markerMovementInc >= roomSizeY) { markerPermitMovement[3] = 0; } //can't go down
 
             //change marker position based on permitted movements
-            if (keyCode === RIGHT_ARROW && markerPermitMovement[1]) {
+            if (keyCode === 68 && markerPermitMovement[1]) {
                 markerPosX = markerPosX + markerMovementInc;
-            } else if (keyCode === LEFT_ARROW && markerPermitMovement[0]) {
+            } else if (keyCode === 65 && markerPermitMovement[0]) {
                 markerPosX = markerPosX - markerMovementInc;
-            } else if (keyCode === UP_ARROW && markerPermitMovement[2]) {
+            } else if (keyCode === 87 && markerPermitMovement[2]) {
                 markerPosY = markerPosY - markerMovementInc;
-            } else if (keyCode === DOWN_ARROW && markerPermitMovement[3]) {
+            } else if (keyCode === 83 && markerPermitMovement[3]) {
                 markerPosY = markerPosY + markerMovementInc;
             }
 
@@ -103,12 +108,12 @@ function drawMarker(xPos, yPos, size) {
 //Draw door in bottom right corner
 function drawDoor() {
     //standard door width ~ 75cm - therefore needs diameter of 150
-    arc(canvasSizeX - 5, canvasSizeY, 150, 150, PI, PI * (2.5 / 2), PIE);
+    arc(roomSizeX - 5, roomSizeY, 150, 150, PI, PI * (2.5 / 2), PIE);
 }
 
 //Click mouse to move to new position
 function mousePressed() {
-    if (mouseX <= canvasSizeX && mouseX >= 0 && mouseY >= 0 && mouseY <= canvasSizeY) {
+    if (mouseX <= roomSizeX && mouseX >= 0 && mouseY >= 0 && mouseY <= roomSizeY) {
         markerPosX = mouseX;
         markerPosY = mouseY;
     }
