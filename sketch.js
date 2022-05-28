73b79c01a714d97390a708981fbc0cf4e48a5ddb
Kat Young <kathryn.ae.young@gmail.com>
2022-05-28 17:00:48 +0100

    Commenting, removal of vis logic
    General tidying and more descriptive comments. Removed some logic for turning vis. on and off as it wasn't really making sense.

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ let canvasSizeY = 380;
 let markerSize = 15; //person head ~15cm wide?
 
 // movement variables
-let markerMovementInc = 2; //3cm increments
+let markerMovementInc = 2; //Xcm increments
 let markerPermitMovement = [1, 1, 1, 1]; //Left right up down
 
 // starting position
@@ -61,27 +61,25 @@ function setup() {
     lowestFreqX = round(calculateLowestRoomModeFreq(roomSizeX), 2);
     lowestFreqY = round(calculateLowestRoomModeFreq(roomSizeY), 2);
 
-    //relating to X-direction generation
+    //setup relating to X-direction oscillator
     oscX.amp(0);
     setOscFreq("X", currentHarmonicX);
     setFreqLabel("X", currentHarmonicX);
 
-    //relating to Y-direction generation
+    //setup relating to Y-direction oscillator
     oscY.amp(0);
     setOscFreq("Y", currentHarmonicY);
     setFreqLabel("Y", currentHarmonicY);
 
+    //setup relating to room labels
     document.getElementById("xSize").innerHTML = str(roomSizeX);
     document.getElementById("ySize").innerHTML = str(roomSizeY);
-
     document.getElementById("lowestMode").innerHTML = str(min(lowestFreqX, lowestFreqY));
 
     ampAnalyser = new p5.Amplitude();
 
-    disableVisualisationToggle();
-
+    //compute the 6 base visualisations 
     computeVisualisations();
-    visXY = createImage(canvasSizeX, canvasSizeY);
 }
 
 
@@ -92,10 +90,10 @@ function draw() {
 
     //draw background visualisation based on current settings
     switch (visStatus) {
-        case 0:
+        case 0: //no vis
             background(255);
             break;
-        case 1:
+        case 1: //yes vis
             switch (currentVisualisation) {
                 case "1":
                     image(visXharm1, 0, 0);
@@ -129,7 +127,7 @@ function draw() {
     line(canvasSizeX / 2 + 5, canvasSizeY / 2, canvasSizeX / 2 - 5, canvasSizeY / 2);
     line(canvasSizeX / 2, canvasSizeY / 2 - 5, canvasSizeX / 2, canvasSizeY / 2 + 5);
 
-    //changing marker location on keyPressed status rather than keyPressed function allows for press and hold
+    //change marker location on keyPressed status - allows for press and hold over using keyPressed()
     if (keyIsPressed) {
         //Use WASD keys to move marker around in increments
         if (keyCode === 65 || keyCode === 68 || keyCode === 87 || keyCode === 83) {
@@ -140,7 +138,7 @@ function draw() {
             if (markerPosY + markerMovementInc >= canvasSizeY) { markerPermitMovement[3] = 0; } //can't go down
 
             //change marker position based on permitted movements
-            if (keyCode === 68 && markerPermitMovement[1]) {
+            if (keyCode === 68 && markerPermitMovement[1]) { // eg if left arrow pressed and marker can go left -> go left
                 markerPosX = markerPosX + markerMovementInc;
             } else if (keyCode === 65 && markerPermitMovement[0]) {
                 markerPosX = markerPosX - markerMovementInc;
@@ -158,12 +156,13 @@ function draw() {
     //draw marker location
     drawMarker(markerPosX, markerPosY, markerSize);
 
-    // calculate new amplitude based on position
+    // calculate new amplitude based on marker position
     ampX = cos(radians(markerPosX / canvasSizeX * currentHarmonicX / 2 * 360));
     ampY = cos(radians(markerPosY / canvasSizeY * currentHarmonicY / 2 * 360));
     oscX.amp(ampX, 0.05);
     oscY.amp(ampY, 0.05);
 
+    // print sound level at 30fps
     if (frameCount % 2 == true) {
         document.getElementById("soundLevel").innerHTML = str(round(ampAnalyser.getLevel(), 2));
     }
@@ -184,68 +183,67 @@ function mousePressed() {
     }
 }
 
-//Button for audio playback for X-direction
+//toggle between playing and pause state for audio playback for X-direction
 function playPauseAudioX() {
-    if (oscStatusX === 0) { // if oscX is off, turn on, enable vis, trigger selection of harmonic and vis
+    if (oscStatusX === 0) { //if oscX is off, turn on, trigger selection of harmonic
         oscX.start();
         oscStatusX = 1;
-        enableVisualisationToggle();
         selectHarmonicX(); 
-    } else if (oscStatusX === 1) { // if oscX is on, turn off, turn off vis unless other osc is on
+    } else if (oscStatusX === 1) { //if oscX is on, turn off
         oscX.stop();
         oscStatusX = 0;
-        if (visStatus & !oscStatusY) {
-            document.getElementById("toggleVisualisation").click();
-            disableVisualisationToggle
-        }
     }
 }
 
-//Button for audio playback for Y-direction
+//toggle between playing and pause state for audio playback for Y-direction
 function playPauseAudioY() {
-    if (oscStatusY === 0) {
+    if (oscStatusY === 0) { //if oscY is off, turn on, trigger selection of harmonic
         oscY.start();
         oscStatusY = 1;
-        enableVisualisationToggle();
-        selectHarmonicY(); //trigger reselection of currentHarmonic and currentVisualisation
-    } else if (oscStatusY === 1) {
+        selectHarmonicY();
+    } else if (oscStatusY === 1) { //if oscY is on, turn off
         oscY.stop();
         oscStatusY = 0;
-        if (visStatus & !oscStatusX) { //is vis is currently on, turn it off too
-            document.getElementById("toggleVisualisation").click();
-            disableVisualisationToggle();
-        }
     }
 }
 
 //Change current harmonic for X-direction
 function selectHarmonicX() {
+    //get radio group element
     var harmonicsX = document.getElementsByName("harmonicSelectRadioX");
 
+    //find active element within radio group
     for (var i = 0; i < harmonicsX.length; i++) {
         if (harmonicsX[i].checked) {
-            currentHarmonicX = harmonicsX[i].value;
-            setCurrentVisualisation();
+            currentHarmonicX = harmonicsX[i].value; //set harmonic based on checked value
+            setCurrentVisualisation(); //set visualisation based on checked value
         }
     }
+
+    //set contents of labels
     setFreqLabel("X", currentHarmonicX * harmonicMultiplier);
     setOscFreq("X", currentHarmonicX * harmonicMultiplier);
 }
 
 //Change current harmonic for Y-direction
 function selectHarmonicY() {
+    //get radio group element
     var harmonicsY = document.getElementsByName("harmonicSelectRadioY");
 
+    //find active element within radio group
     for (var i = 0; i < harmonicsY.length; i++) {
         if (harmonicsY[i].checked) {
-            currentHarmonicY = harmonicsY[i].value;
-            setCurrentVisualisation();
+            currentHarmonicY = harmonicsY[i].value;//set harmonic based on checked value
+            setCurrentVisualisation(); //set visualisation based on checked value
         }
     }
+
+    //set contents of labels
     setFreqLabel("Y", currentHarmonicY * harmonicMultiplier);
     setOscFreq("Y", currentHarmonicY * harmonicMultiplier);
 }
 
+//set frequency of oscillator - calculated from lowest freq and current harmonic, flag for X or Y
 function setOscFreq(oscIndicator, harmonic) {
     switch (oscIndicator) {
         case "X":
@@ -257,6 +255,7 @@ function setOscFreq(oscIndicator, harmonic) {
     }
 }
 
+//set contents of frequency labe - calculated from lowest freq and current harmonic, flag for X or Y
 function setFreqLabel(labelIndicator, harmonic) {
     switch (labelIndicator) {
         case "X":
@@ -268,6 +267,7 @@ function setFreqLabel(labelIndicator, harmonic) {
     }
 }
 
+//calculate frequency of first standing wave for given distance
 function calculateLowestRoomModeFreq(dimension) {
     var c = 343;
     var mode = 1;
@@ -277,6 +277,7 @@ function calculateLowestRoomModeFreq(dimension) {
 
 }
 
+//reset state of all UI elements
 function resetAll() {
 
     //reset X
@@ -330,8 +331,9 @@ function multiplyHarmonics() {
 
 //calculate 6 visualisations for harmonic 1-3, x and y directions
 function computeVisualisations() {
+    //uses same maths as amplitude calculation
 
-    //calculate colour values for harmonic 1-3, x direction. creates [3x681]
+    //calculate colour values for harmonics 1-3, x direction. creates [3x681]
     for (var j = 0; j < 3; j++) {
         colValueX[j] = [];
         for (var i = 0; i <= canvasSizeX; i++) {
@@ -339,7 +341,7 @@ function computeVisualisations() {
         }
     }
 
-    //calculate colour values for harmonic 1-3, y direction. creates [3x381]
+    //calculate colour values for harmonics 1-3, y direction. creates [3x381]
     for (var j = 0; j < 3; j++) {
         colValueY[j] = [];
         for (var i = 0; i <= canvasSizeY; i++) {
@@ -351,9 +353,9 @@ function computeVisualisations() {
     visXharm1 = createImage(canvasSizeX, canvasSizeY);
     visXharm1.loadPixels();
     let x, y, currentValue;
-    // fill with color
-    for (y = 0; y < visXharm1.height; y++) {
-        for (x = 0; x < visXharm1.width; x++) {
+    // fill with color as defined by initial vector of color values
+    for (y = 0; y < canvasSizeY; y++) {
+        for (x = 0; x < canvasSizeX; x++) {
             currentValue = colValueX[0][x];
             writeColor(visXharm1, x, y, round(currentValue * startingColorX[0]), round(currentValue * startingColorX[1]), round(currentValue * startingColorX[2]), 255);
         }
@@ -363,9 +365,9 @@ function computeVisualisations() {
     //create image for x harm 2
     visXharm2 = createImage(canvasSizeX, canvasSizeY);
     visXharm2.loadPixels();
-    // fill with color
-    for (y = 0; y < visXharm2.height; y++) {
-        for (x = 0; x < visXharm2.width; x++) {
+    // fill with color as defined by initial vector of color values
+    for (y = 0; y < canvasSizeY; y++) {
+        for (x = 0; x < canvasSizeX; x++) {
             currentValue = colValueX[1][x];
             writeColor(visXharm2, x, y, round(currentValue * startingColorX[0]), round(currentValue * startingColorX[1]), round(currentValue * startingColorX[2]), 255);
         }
@@ -375,9 +377,9 @@ function computeVisualisations() {
     //create image for x harm 3
     visXharm3 = createImage(canvasSizeX, canvasSizeY);
     visXharm3.loadPixels();
-    // fill with color
-    for (y = 0; y < visXharm3.height; y++) {
-        for (x = 0; x < visXharm3.width; x++) {
+    // fill with color as defined by initial vector of color values
+    for (y = 0; y < canvasSizeY; y++) {
+        for (x = 0; x < canvasSizeX; x++) {
             currentValue = colValueX[2][x];
             writeColor(visXharm3, x, y, round(currentValue * startingColorX[0]), round(currentValue * startingColorX[1]), round(currentValue * startingColorX[2]), 255);
         }
@@ -387,9 +389,9 @@ function computeVisualisations() {
     //create image for Y harm 1
     visYharm1 = createImage(canvasSizeX, canvasSizeY);
     visYharm1.loadPixels();
-    // fill with color
-    for (y = 0; y < visYharm1.height; y++) {
-        for (x = 0; x < visYharm1.width; x++) {
+    // fill with color as defined by initial vector of color values
+    for (y = 0; y < canvasSizeY; y++) {
+        for (x = 0; x < canvasSizeX; x++) {
             currentValue = colValueY[0][y];
             writeColor(visYharm1, x, y, round(currentValue * startingColorY[0]), round(currentValue * startingColorY[1]), round(currentValue * startingColorY[2]), 255);
         }
@@ -399,9 +401,9 @@ function computeVisualisations() {
     //create image for Y harm 2
     visYharm2 = createImage(canvasSizeX, canvasSizeY);
     visYharm2.loadPixels();
-    // fill with color
-    for (y = 0; y < visYharm2.height; y++) {
-        for (x = 0; x < visYharm2.width; x++) {
+    // fill with color as defined by initial vector of color values
+    for (y = 0; y < canvasSizeY; y++) {
+        for (x = 0; x < canvasSizeX; x++) {
             currentValue = colValueY[1][y];
             writeColor(visYharm2, x, y, round(currentValue * startingColorY[0]), round(currentValue * startingColorY[1]), round(currentValue * startingColorY[2]), 255);
         }
@@ -411,17 +413,20 @@ function computeVisualisations() {
     //create image for Y harm 3
     visYharm3 = createImage(canvasSizeX, canvasSizeY);
     visYharm3.loadPixels();
-    // fill with color
-    for (y = 0; y < visYharm3.height; y++) {
-        for (x = 0; x < visYharm3.width; x++) {
+    // fill with color as defined by initial vector of color values
+    for (y = 0; y < canvasSizeY; y++) {
+        for (x = 0; x < canvasSizeX; x++) {
             currentValue = colValueY[2][y];
             writeColor(visYharm3, x, y, round(currentValue * startingColorY[0]), round(currentValue * startingColorY[1]), round(currentValue * startingColorY[2]), 255);
         }
     }
     visYharm3.updatePixels();
+
+    // create image for building combination visualisation in later
+    visXY = createImage(canvasSizeX, canvasSizeY);
 }
 
-// helper function for writing colour value to array
+// helper function for writing color value to array
 function writeColor(image, x, y, red, green, blue, alpha) {
     let index = (x + y * width) * 4;
     image.pixels[index] = red;
@@ -466,7 +471,7 @@ function computeCurrentXYVisualisation() {
 
 function setCurrentVisualisation() {
     if (oscStatusX & !oscStatusY) {
-        currentVisualisation = currentHarmonicX; //if only oscX, use 1-3
+        currentVisualisation = str(currentHarmonicX); //if only oscX, use 1-3
     } else if (!oscStatusX & oscStatusY) {
         currentVisualisation = str(int(currentHarmonicY) + 3); //if only oscY, use 4-6
     } else if (oscStatusX & oscStatusY) { //if both, use "XY" and recompute
@@ -475,6 +480,7 @@ function setCurrentVisualisation() {
     }
 }
 
+//toggle status for visualisation on / off
 function toggleVisualisation() {
     if (visStatus == 0) { //turn on visualisation
         visStatus = 1;
@@ -483,10 +489,12 @@ function toggleVisualisation() {
     }
 }
 
+//disable visualisation toggle
 function disableVisualisationToggle() {
     document.getElementById("toggleVisualisation").disabled = true;
 }
 
+//enable visualisation toggle
 function enableVisualisationToggle() {
     document.getElementById("toggleVisualisation").disabled = false;
 }
